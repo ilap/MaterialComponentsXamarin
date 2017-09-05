d70e6dd50ec5321481d2749cdbfa7edfe148d343
Pal Dorogi ilap <pal.dorogi@gmail.com>
2017-09-05 19:16:12 +1000

    Fixed InkOverLay and deleted temporary files

--- samples/ShrineRemoteImage/Views/ShrineInkOverlay.cs
+++ samples/ShrineRemoteImage/Views/ShrineInkOverlay.cs
@@ -2,6 +2,8 @@
 using MaterialComponents.MaterialInk;
 using CoreGraphics;
 using UIKit;
+using Foundation;
+
 namespace ShrineRemoteImage.iOS.View
 {
     
@@ -11,21 +13,38 @@ namespace ShrineRemoteImage.iOS.View
 
         public ShrineInkOverlay(CGRect frame) : base (frame)
         {
-            var cyan = new UIColor(22 / 255, 240 / 255, 240 / 255, 0.2f);
-            inkTouchController = new MDCInkTouchController(this);
-            inkTouchController.DefaultInkView.InkColor = cyan;
-            inkTouchController.AddInkView();
-            // FIXME: inkTouchController.Delegate = (IMDCInkTouchControllerDelegate)this;
-
+            InitTouchController();
         }
 
         public ShrineInkOverlay() : base() {
-            
+            InitTouchController();
         }
 
+        void InitTouchController() {
+            var cyan = new UIColor(22 / 255f, 240 / 255f, 240 / 255f, 0.2f);
+            inkTouchController = new MDCInkTouchController(this);
+            inkTouchController.DefaultInkView.InkColor = cyan;
+            inkTouchController.AddInkView();
+            inkTouchController.Delegate = new ShrineInkOverlayDelegate();
+        }
+
+        [Export("layoutSubviews")]
         public override void LayoutSubviews()
         {
             base.LayoutSubviews();
+        }
+
+        public class ShrineInkOverlayDelegate : MDCInkTouchControllerDelegate {
+            
+            public ShrineInkOverlayDelegate (): base () {
+                Console.WriteLine("ShrineInkOverlayDelegate was called\n");
+
+            }
+
+            // XXX: InkOverLay Example
+            // public override bool ShouldProcessInkTouchesAtTouchLocation(MDCInkTouchController inkTouchController, CGPoint location) {
+            //    return true;
+            //}
         }
     }
 }
